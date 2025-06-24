008883defe9a81f9d739222dc30341a2e2ca7fa2
sahil <sahil.malik@thewasserstoff.com>
2025-06-24 18:59:27 +0530

    get route

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 // Routes
+app.get("/", (req, res) => {
+  return res.send("API is running...");
+});
 app.use("/api/waitlist", waitlistRoutes);
 
 // Start server
