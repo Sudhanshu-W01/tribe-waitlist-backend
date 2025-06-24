3883ec5f5461be25a05e903276b1ef117b3bd7bc
sahil <sahil.malik@thewasserstoff.com>
2025-06-24 21:05:27 +0530

    remove college required from model

--- controllers/waitlistController.js
+++ controllers/waitlistController.js
@@ -6,19 +6,24 @@ export const joinWaitlist = async (req, res) => {
 
     // Basic validation
     if (!fullname || !role || !email) {
-      return res.status(400).json({ message: "All fields are required" });
+      return res.status(400).json({ message: "All fields are required." });
+    }
+    if (role === "Student" && !collegeName) {
+      return res
+        .status(400)
+        .json({ message: "College name is required for students." });
     }
 
     // Check for duplicate email
     const existing = await Waitlist.findOne({ email });
     if (existing) {
-      return res.status(409).json({ message: "Email already on waitlist" });
+      return res.status(409).json({ message: "Email already on waitlist." });
     }
 
     const newEntry = new Waitlist({ fullname, role, collegeName, email });
     await newEntry.save();
 
-    res.status(201).json({ message: "Successfully joined the waitlist" });
+    res.status(201).json({ message: "Successfully joined the waitlist." });
   } catch (err) {
     console.error("Error joining waitlist:", err);
     res.status(500).json({ message: "Server error" });
--- models/Waitlist.js
+++ models/Waitlist.js
@@ -4,7 +4,7 @@ const waitlistSchema = new mongoose.Schema(
   {
     fullname: { type: String, required: true },
     role: { type: String, required: true },
-    collegeName: { type: String, required: true },
+    collegeName: { type: String, required: false },
     email: { type: String, required: true, unique: true },
   },
   {
